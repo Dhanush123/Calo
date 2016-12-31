c54d8d3af8779bedd9bbed2ff45fa1fac5367e31
Dhanush123 <dhanush.patel@ymail.com>
2016-12-31 03:24:26 -0800

    hope this work
    now

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ const request = require("request");
 const event = require("search-eventbrite");
 const moment = require("moment-timezone");
 const Yelp = require('yelp-api-v3');
-var EventSearch = require("facebook-events-by-location-core");
+const EventSearch = require("facebook-events-by-location-core");
 
 var yelp = new Yelp({
   app_id: "g4GGJOk6feS7HoSDAfzJtw",
   app_secret: "tIJAG3oWzXTA9YJUJunB4DrDqwFtYbrGR6BrICG2U0lWk05ucvT8gvlesvrhZElt"
 });
+
+var publicConfig = {
+    key: 'AIzaSyCKzerWst-Rwanyum59N3J60yruUsIUe-k',
+    stagger_time:       100, // for elevationPath
+    encode_polylines:   false,
+    secure:             true, // use https
+};
+var gmAPI = new GoogleMapsAPI(publicConfig);
 
 const restService = express();
 restService.use(bodyParser.json());
@@ -26,10 +34,25 @@ restService.get("/p", function (req, res) {
   console.log("hook request");
   try {
       if (req) {
-        if(req.query.serq){
+        if(req.query.fb == "true"){
+          getFBEvents(req, function(result) {
+                     //callback is ultimately to return Messenger appropriate responses formatted correctly
+                     console.log("results w/ getFBEvents: ", cardsSend);
+                     if(cardsSend){
+                       return res.json({
+                         results: cardsSend,
+                       });
+                     }
+                     else{
+                       return res.json({
+                         err: "NOCARDSFOUND"
+                       });
+                     }
+                   });
+        }
+        else if(req.query.serq){
           getNearbyEventsBrite(req, function(result) {
                      //callback is ultimately to return Messenger appropriate responses formatted correctly
-                     var result;
                      console.log("results w/ getNearbyEventsBrite: ", cardsSend);
                      if(cardsSend){
                        return res.json({
@@ -46,7 +69,6 @@ restService.get("/p", function (req, res) {
         else if(req.query.yerq){
           getYelpEvents(req, function(result) {
                      //callback is ultimately to return Messenger appropriate responses formatted correctly
-                     var result;
                      console.log("results w/ getYelpEvents: ", cardsSend);
                      if(cardsSend){
                        return res.json({
@@ -97,7 +119,8 @@ function YelpCall(callback){
     console.log("data pre-JSONparse: "+data);
     var data = JSON.parse(data);
     console.log("data: "+data);
-    if(data.total >= 5){
+    if(data.total > 0){
+      var lim = data.total >= 5 ? 5 : data.total;
       for(var i = 0; i < 5; i++){
         if(data.businesses[i]){
           var cardObj = {
@@ -152,7 +175,8 @@ function EventbriteCall(callback) {
       else{
         console.log("events: ", events);
         if(events.length > 0){
-          for(var i = 0; i < 5; i++){
+          var lim = events.length >= 5 ? 5 : events.length;
+          for(var i = 0; i < lim; i++){
             if(events[i]){
               var cardObj = {
                 title: "",
@@ -175,6 +199,65 @@ function EventbriteCall(callback) {
         }
         callback();
       }
+  });
+}
+
+function getFBEvents(callback) {
+  var params = {
+    'address': cityName,
+    'components': 'components=country:US',
+    'language':   'en',
+    'region':     'us'
+  };
+
+  gmAPI.geocode(params, function(err, result) {
+    console.log('err: '+err);
+    console.log('result: '+result);
+    var propValue;
+    for(var propName in result) {
+        propValue = result[propName]
+        console.log(propName,propValue);
+    }
+    if(err == null && result.status == 'OK'){
+      if(result.results[0].geometry.location != undefined) {
+        console.log('result.results[0]: ' + result.results[0]);
+        console.log('result.results[0].geometry.location: ' + result.results[0].geometry.location);
+        var lat = result.results[0].geometry.location.lat
+        var long = result.results[0].geometry.location.lng;
+        console.log('result.results[0].geometry.location.lat: ' + lat);
+        console.log('result.results[0].geometry.location.lng: ' + long);
+        var es = new EventSearch({
+            "lat": lat,
+            "lng": long
+        });
+        es.search().then(function (eventss) {
+            console.log(JSON.stringify(eventss));
+            var res = JSON.parse(eventss);
+            if(res.metadata.events > 0){
+              var lim = res.metadata.events >= 5 ? 5 : res.metadata.events;
+              for(var i = 0; i < lim; i++){
+                if(events[i]){
+                  var cardObj = {
+                    title: "",
+                    image_url: "",
+                    subtitle: "",
+                  };
+                  cardObj.title = res.events[i].name;
+                  cardObj.image_url = res.events[i].coverPicture;
+                  cardObj.subtitle = moment(res.events[i].startTime).format("MMMM Do YYYY");
+                  cardsSend[i] = cardObj;
+                }
+              }
+            }
+            callback();
+        }).catch(function (error) {
+            console.error(JSON.stringify(error));
+        });
+      }
+    }
+    else {
+      callback();
+    }
   });
 }
 
